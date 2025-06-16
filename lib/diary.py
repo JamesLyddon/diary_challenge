a1873f6b712a9605546279a94c96979b9e70a116
james lyddon <jameslyddon@gmail.com>
2025-06-16 16:58:35 +0100

    complete

--- lib/diary.py
+++ lib/diary.py
@@ -1,3 +1,4 @@
+import re
 from lib.entry import Entry
 from lib.task import Task
 from lib.contact import Contact
@@ -9,7 +10,37 @@ class Diary:
         self.contacts = []
 
     def add_entry(self, entry):
+        self.extract_contact(entry)
         self.entries.append(entry)
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def extract_contact(self, entry):
+        matches = re.findall(r"(?:\b([A-Z][a-z]+):\s(\d{3}-\d{3}-\d{4}))|(?:call\s([A-Z][a-z]+)\s(\d{11}))"
+, entry.content)
+        for match in matches:
+        # Each match is a tuple of 4, but only 2 fields will be non-None
+            name = match[0] or match[2]
+            number = match[1] or match[3]
+            self.contacts.append({'name': name, 'number': number})
 
     def get_all_entries(self):
         return self.entries
+    
+    def get_all_tasks(self):
+        return self.tasks
+    
+    def get_all_contacts(self):
+        return self.contacts
+    
+    def get_suitable_entry(self, wpm, mins):
+        best_fit = None
+        current_longest = 0
+        max_words = wpm * mins
+        for entry in self.entries:
+            length = len(entry.content.split())
+            if current_longest < length <= max_words:
+                current_longest = length
+                best_fit = entry
+        return best_fit
--- tests/diary_integration_test.py
+++ tests/diary_integration_test.py
@@ -12,12 +12,6 @@ Given a Diary
 We can add entries
 And retrieve those entries
 """
-# diary = Diary()
-# entry_1 = Entry('title_1', 'content_1')
-# entry_2 = Entry('title_2', 'content_2')
-# diary.add_entry(entry_1)
-# diary.add_entry(entry_2)
-# diary.get_all_entries() # => [entry_1, entry_2]
 def test_add_retrieve_entries():
     diary = Diary()
     entry_1 = Entry('title_1', '1 2 3')
@@ -26,30 +20,33 @@ def test_add_retrieve_entries():
     diary.add_entry(entry_2)
     assert diary.get_all_entries() == [entry_1, entry_2]
 
-
-# """
-# Given a Diary
-# We can add tasks
-# And retrieve those tasks
-# """
-# diary = Diary()
-# task_1 = Task('description_1')
-# task_2 = Task('description_2')
-# diary.add_task(task_1)
-# diary.add_task(task_2)
-# diary.get_all_tasks() # => [task_1, task_2]
+"""
+Given a Diary
+We can add tasks
+And retrieve those tasks
+"""
+def test_add_tasks():
+    diary = Diary()
+    task_1 = Task('walk the dog')
+    task_2 = Task('feed the cat')
+    diary.add_task(task_1)
+    diary.add_task(task_2)
+    assert diary.get_all_tasks() == [task_1, task_2]
 
 # """
 # Given a Diary
 # We can add entries with phonenumber and they will be parsed and saved
 # We can then retrieve any saved contact details
 # """
-# diary = Diary()
-# entry_1 = Entry('title_1', 'content_1 Pete: 570-560-7834')
-# entry_2 = Entry('title_2', 'content_2')
-# diary.add_entry(entry_1)
-# diary.add_entry(entry_2)
-# diary.get_all_contacts() # => [contact_1]
+def test_phone_numbers_scraped_from_entries():
+    diary = Diary()
+    entry_1 = Entry('title_1', 'content_1 Pete: 570-560-7834')
+    entry_2 = Entry('title_2', 'content_2')
+    entry_3 = Entry('title_2', 'content_3 call Sally 07986756243')
+    diary.add_entry(entry_1)
+    diary.add_entry(entry_2)
+    diary.add_entry(entry_3)
+    assert diary.get_all_contacts() == [{'name': 'Pete', 'number': '570-560-7834'}, {'name': 'Sally', 'number': '07986756243'}]
 
 # """
 # Given a Diary
@@ -57,9 +54,16 @@ def test_add_retrieve_entries():
 # We can provide a time and wpm
 # And get back the longest entry we can read in that time
 # """
-# diary = Diary()
-# entry_1 = Entry('title_1', '1 2 3 4 5')
-# entry_2 = Entry('title_2', '1 2 3')
-# diary.add_entry(entry_1)
-# diary.add_entry(entry_2)
-# diary.get_entry(1, 3) # => entry_2
+def test_get_suitable_entry():
+    diary = Diary()
+    entry_1 = Entry('title_1', '1 2 3')
+    entry_2 = Entry('title_2', '1 2 3 4 5')
+    entry_3 = Entry('title_2', '1 2 3 4 5 6 7 8 9 10')
+    diary.add_entry(entry_1)
+    diary.add_entry(entry_2)
+    diary.add_entry(entry_3)
+    assert diary.get_suitable_entry(1, 3) == entry_1
+    assert diary.get_suitable_entry(1, 5) == entry_2
+    assert diary.get_suitable_entry(1, 6) == entry_2
+    assert diary.get_suitable_entry(1, 10) == entry_3
+    assert diary.get_suitable_entry(1, 11) == entry_3
